557fba0b70513d9c502905048db85ed9160b3f1d
Yuriy Borys <iam618384@gmail.com>
2019-10-10 23:49:33 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import nanoid from 'nanoid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class Passgen extends React.Component {
+    state = {
+        password: '',
+    }
+
+    generator = () => {
+        this.setState({
+            password: nanoid(),
+        })
+    }
+
+    render() {
+        let {password} = this.state;
+        return (
+            <div>
+                <label>Password:</label>
+                {' '}
+                <input type="text" value={password} />
+                {' '}
+                <button onClick={this.generator}>Generate</button>
+            </div>
+        )
+    }
 }
-
-export default App;
